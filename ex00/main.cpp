4d89f4d5bcf557f7b3d57fe49726ee4577af3cf0
Rbs <rubsanch@student.42madrid.com>
2026-02-14 10:18:15 +0100

    before 1st compile

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,8 +6,21 @@
 /*   By: rubsanch <rubsanch@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/14 09:24:56 by rubsanch          #+#    #+#             */
-/*   Updated: 2026/02/14 09:26:59 by rubsanch         ###   ########.fr       */
+/*   Updated: 2026/02/14 10:18:01 by rubsanch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
 
+#include <iostream>
+int main( void ) {
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
+
+	c = b;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return 0;
+}
